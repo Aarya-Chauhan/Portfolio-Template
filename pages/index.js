53e18242a6207a8641b93a79d193f4d5ce6d598f
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-17 16:39:33 +0530

    adding framer motion

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+// next image
+import Image from "next/image";
+
+//components
+import ParticlesContainer from "../components/ParticlesContainer";
+
+
+
 const Home = () => {
-  return <div>Home</div>;
+  // return <div>Home</div>;
 };
 
 export default Home;
