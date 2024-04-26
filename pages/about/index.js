108a0f1ef20da0420512f0961a3d59209bd9765b
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-26 21:31:06 +0530

    updations in about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -90,19 +90,40 @@ export const aboutData = [
 
 import Avatar from  '../../components/Avatar';
 import Circles from '../../components/Circles';
-import {motion} from 'framer-motion';
+import {interpolate, motion} from 'framer-motion';
 import {fadeIn} from '../../variants';
 
 
 import { useState } from "react";
 
 const About = () => {
-  const [index] = useState(0)
-  return <div className="">
+  const [index, setIndex] = useState(0);
+  console.log(index)
+  return <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
     <Circles className=''/>
-    <motion.div className="hidden xl:flex  absolute bottom-0 -left-[370px]">
+    <motion.div variants={fadeIn('right' , 0.2)} initial='hidden' animate='show' exit='hidden' className="hidden xl:flex  absolute bottom-0 -left-[370px]">
       <Avatar/>
     </motion.div>
+    <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+      <div>text</div>
+      <div>
+        <div className="flex gap-x-4 xl:gap-x-8 mx-autoxl:mx-0 mb-4">
+          {aboutData.map((item, itemIndex)=>{
+            return (
+              <div key={itemIndex} className={` ${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+              onClick={()=>setIndex(itemIndex)}> {item.title}</div>
+            )
+          })}
+        </div>
+        <div>
+          {aboutData[index].info.map((item,itemIndex)=>{
+            return <div key={itemIndex}>
+              <div> {item.title} </div>
+            </div>
+          })}
+        </div>
+      </div>
+    </div>
     
   </div>
 };
