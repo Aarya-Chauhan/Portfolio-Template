3097a3147738639ae7999968d31e4d5ff468dba0
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-04-30 22:48:37 +0530

    kuch bhi

--- pages/about/index.js
+++ pages/about/index.js
@@ -166,10 +166,10 @@ const About = () => {
         <div className=" py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
           {aboutData[index].info.map((item,itemIndex)=>{
             return <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gapx2 items-center text-white/60">
-              <div key={itemIndex} className="font-light mb-2 md:mb-0 "> {item.title} </div>
-              <div key={itemIndex} className="hidden md:flex"> : </div>
-              <div key={itemIndex}>{item.stage}</div>
-              <div key={itemIndex} className="flex gap-x-4">
+              <div className="font-light mb-2 md:mb-0 "> {item.title} </div>
+              <div className="hidden md:flex"> : </div>
+              <div>{item.stage}</div>
+              <div className="flex gap-x-4">
                 {item.icons?.map((icon,itemIndex)=>{
                   return <div key={itemIndex} className="text-2xl text-white"> {icon} </div>
                 })}
